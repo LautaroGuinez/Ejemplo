a44fdb4a8f99bce2af4b7c931ecbbeaccc6d3023
LautaroGuinez <laucha_09@live.com>
2023-02-25 18:48:19 -0300

    Se sube el proyecto

---
+++ codigo.js
@@ -0,0 +1,6 @@
+function sumar() {
+    const num1 = parseInt(document.getElementById("num1").value);
+    const num2 = parseInt(document.getElementById("num2").value);
+    const resultado = num1 + num2;
+    alert("La suma es: " + resultado);
+  }
